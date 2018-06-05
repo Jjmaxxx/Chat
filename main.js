c186c3455cadd03e9d24af2aebf9fded90744039
Justin <justlee888@gmail.com>
2018-06-04 18:54:06 -0700

    make it better

---
+++ main.js
@@ -0,0 +1,36 @@
+let express = require("express");
+let app = express();
+let http = require("http").Server(app);
+let history = [];
+let port = process.env.PORT || 3000;
+let path = require('path');
+let server = require("http").Server(app);
+let io = require("socket.io")(server);
+
+app.use(express.static(path.resolve(__dirname, './client')));
+
+app.get("/",(req,res)=>{
+  res.sendFile("C:/Users/Justin/Documents/Chat/index.html")
+});
+
+
+server.listen(port,()=>{
+  console.log("alright")
+})
+io.on("connection",(socket)=>{
+    socket.emit("chat",history);
+  socket.on("chat",(name, result)=>{
+    history.push([name, result]);
+    io.sockets.emit("chat",history);
+    io.sockets.emit("typing","")
+
+  })
+  socket.on("typing",(result)=>{
+    if (result == ""){
+      io.sockets.emit("typing", "")
+
+    } else{
+      io.sockets.emit("typing",result + " is typing a message")
+    }
+  });
+})
